c62f56c5ac0d41e6ca4033776bc457bcd094ce74
angelq <angelquimorita@gmail.com>
2024-02-01 16:38:16 -0700

    Se añadió la actualización a tiempo real de votos. Se encontró un error: Si se insertan 5 veces seguidas, la página web se detiene. Arreglar si es posible

--- public/grafica.js
+++ public/grafica.js
@@ -3,17 +3,53 @@ const colors = ['#862029', '#bc6235'];
 
 const graph = document.querySelector("#grafica");
 
-const data = {
-    labels: labels,
-    datasets: [{
-        data: [50, 50],
-        backgroundColor: colors,
-    }]
-};
+const [btnVenados, btnNaranjeros] = document.querySelectorAll('.contenido-boton');
 
-const config = {
-    type: 'pie',
-    data: data,
-};
+let myChart = null;
 
-new Chart(graph, config);
+const get_data = async () => {
+    const response = await fetch('/get_initial_votes');
+    const dataVotos = await response.json();
+    console.log(dataVotos)
+    return [dataVotos.votes.team1, dataVotos.votes.team2];
+}
+
+const create_chart = async () => {
+    const chart_data = await get_data();
+
+    const data = {
+        labels: labels,
+        datasets: [{
+            data: chart_data,
+            backgroundColor: colors,
+        }]
+    };
+
+    const config = {
+        type: 'pie',
+        data: data,
+    };
+
+     myChart = new Chart(graph, config);
+}
+
+const update_chart = async () => {
+    const chart_data = await get_data();
+    myChart.data = {
+        labels,
+        datasets: [
+            {
+                data: chart_data,
+                backgroundColor: colors,
+            }
+        ]
+    }
+    myChart.update();
+    document.querySelector('#count_1').innerHTML = chart_data[0];
+    document.querySelector('#count_2').innerHTML = chart_data[1];
+}
+
+create_chart();
+btnVenados.addEventListener('click', update_chart);
+btnNaranjeros.addEventListener('click', update_chart);
+
